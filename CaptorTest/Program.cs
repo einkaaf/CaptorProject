23260d3c20151e3d17e05a1bfa6ffd753118518c
Erfan Kazemi <erfankm7@gmail.com>
2022-06-25 12:55:40 +0430

    Add Simple Test Project

---
+++ CaptorTest/Program.cs
@@ -0,0 +1,6 @@
+﻿using Captor.Response;
+using Captor.Service;
+
+CaptorResponse captorResponse = TextorBuilder.Init().UseCustomSize(50, 140).AddHardness(10).UseBasicRotation(true).Build();
+string result = Convert.ToBase64String(captorResponse.Image);
+Console.WriteLine("aa");
